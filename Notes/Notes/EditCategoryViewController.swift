ceb47e76a417732a96d0fe144c1ffd39ce5254c8
Ben Larrabee <ben.larrabee@interapthq.com>
2016-10-25 22:10:28 -0400

    Tuesday, 22:10

---
+++ Notes/Notes/EditCategoryViewController.swift
@@ -0,0 +1,76 @@
+//
+//  EditCategoryViewController.swift
+//  Notes
+//
+//  Created by Ben Larrabee on 10/25/16.
+//  Copyright © 2016 Ben Larrabee. All rights reserved.
+//
+
+import UIKit
+
+class EditCategoryViewController: UIViewController {
+
+  @IBOutlet weak var editCategoryBGColor: UIView!
+  @IBOutlet weak var editCategoryName: UITextField!
+  @IBOutlet weak var editRedBGColor: UISlider!
+  @IBOutlet weak var editGreenBGColor: UISlider!
+  @IBOutlet weak var editBlueBGColor: UISlider!
+  let defaults = UserDefaults.standard
+  
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let colorData = defaults.data(forKey: "editBGColor")
+    if let colorData = colorData {
+      editCategoryBGColor.backgroundColor = NSKeyedUnarchiver.unarchiveObject(with: colorData) as? UIColor ?? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    } else {
+      editCategoryBGColor.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+    }
+    let BGColor = CIColor(color: editCategoryBGColor.backgroundColor!)
+    editRedBGColor.value = Float(BGColor.red)
+    editGreenBGColor.value = Float(BGColor.green)
+    editBlueBGColor.value = Float(BGColor.blue)
+    
+        // Do any additional setup after loading the view.
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    
+    let colorData = NSKeyedArchiver.archivedData(withRootObject: editCategoryBGColor.backgroundColor!)
+    defaults.set(colorData, forKey: "editBGColor")
+
+    defaults.synchronize()
+  }
+
+  
+  
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+  
+  
+  
+  
+  
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+  @IBAction func xColorChange(_ sender: AnyObject) {
+    editCategoryBGColor.backgroundColor = UIColor(red: CGFloat(editRedBGColor.value), green: CGFloat(editGreenBGColor.value), blue: CGFloat(editBlueBGColor.value), alpha: 1.0)
+  
+  }
+  
+  
+}
